51ea2ac64cc7223dcc69e26cfd0180ba379f3d86
Sawai Chungsri <sawai.chungsri@codebox.tech>
2018-11-15 19:06:38 +0700

    - added laravel authentication - implement multiple role authentication

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth']], function () {
+	Route::get('/home', 'HomeController@index');
+});
+
+Route::group(['prefix' => 'admin'], function(){
+	Route::group(['middleware' => ['admin']], function(){
+		Route::get('/dashboard', 'admin\AdminController@dashboard');
+	});
+});
